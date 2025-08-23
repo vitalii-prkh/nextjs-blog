45193dd4a03ae456e963a45ac3fbf57b0f534e37
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-08-23 11:44:20 +0300

    Enhance post and home pages with dynamic content rendering and date formatting, add custom 404 page, and integrate `date-fns` library.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import {getSortedPostsData} from '../lib/posts';
 import Layout, {siteTitle} from '../components/layout';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 
 export default function Home({allPostsData}) {
@@ -21,11 +23,11 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
